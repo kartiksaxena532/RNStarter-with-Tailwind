4a4772dbcf283de0040b2216325dde5bc3f47a5e
Kartik Saxena <karrtiksaxena@gmail.com>
2024-04-13 01:36:00 +0530

    starter template with tailwind

---
+++ app/_layout.jsx
@@ -0,0 +1,19 @@
+//rnfes react native function component in style basically stylesheet wala component snippet
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+ 
+import {Slot, Stack } from "expo-router"
+
+
+const RootLayout = () => {
+  return (
+
+      <Stack>
+        <Stack.Screen name="index" options={{headerShown:  false}} />
+   
+      </Stack>
+  )
+}
+
+export default RootLayout
---
+++ app/index.jsx
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import {Link} from "expo-router"
+
+
+export default function App() {
+  return (
+        <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Kartik App</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{color:"blue"}}>
+        Go to profile
+      </Link>
+      </View>
+  );
+}
+
