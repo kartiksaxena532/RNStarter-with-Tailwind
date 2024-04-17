9f98a655b018e2ea840bb5c50cb489588cac890c
Kartik Saxena <karrtiksaxena@gmail.com>
2024-04-17 14:15:04 +0530

    sign in started

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,12 +1,12 @@
 import { Text, View } from 'react-native'
 import React from 'react'
+import {SafeAreaView} from "react-native-safe-area-context"
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn
-      </Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+<Text>Sign in</Text>
+    </SafeAreaView>
   )
 }
 
