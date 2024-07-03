df3eeb887810c217e7df4e92a17be1dfae020a7f
Kartik Saxena <karrtiksaxena@gmail.com>
2024-07-04 03:04:20 +0530

    added new reanimated library

--- app/profile.jsx
+++ app/profile.jsx
@@ -1,10 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text,Button } from 'react-native'
 import React from 'react'
-
+import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
 const Profile = () => {
+
+
+  const width = useSharedValue(100);
+
+  const handlePress = () => {
+    width.value = withSpring(width.value + 50);
+  };
+
+  
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className="flex-1 items-center justify-center">
+        <Animated.View
+        style={{
+          width,
+          height: 100,
+          backgroundColor: 'violet',
+          marginBottom:10,
+        }}
+      />
+      <Button onPress={handlePress} title="Click me" />
+    
     </View>
   )
 }
